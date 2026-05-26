1777b673fbf251961ba3acc6427ecb1847008bcd
deepikasrm5 <vaishnavisriram76@gmail.com>
2026-05-26 10:30:57 +0530

    Added locators for homepage

--- pages/home-page.js
+++ pages/home-page.js
@@ -1,5 +1,11 @@
 export class HomePage {
     constructor(page) {
         this.page = page;
+
+        this.homePageTitle = page.getByRole('heading', { name: 'Playwright Test Suite' });
+        this.homePageDescription = page.locator('div.text-center mb-12').locator('p');
+        this.sectionCard = (section) => page.getByTestId(`section-${section}`);
+        this.openTestSectionButton = (section) => this.sectionCard(section).getByRole('button', { name: 'Open Test Section' });
+        this.backButton = page.getByTestId('back-button');
     }
 }
