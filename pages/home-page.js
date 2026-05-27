be62973a8d70ceaca7bc183f9682b39b9cb18582
deepikasrm5 <vaishnavisriram76@gmail.com>
2026-05-27 19:20:34 +0530

    added all testcases for homepage

--- pages/home-page.js
+++ pages/home-page.js
@@ -5,10 +5,12 @@ class HomePage {
 
         this.homePageTitle = page.getByRole('heading', { name: 'Playwright Test Suite' });
         this.homePageHeader = page.getByText('Comprehensive Test Website')
-        this.homePageDescription = page.locator('div.text-center mb-12').locator('p');
-        this.sectionCard = (section) => page.getByTestId(`section-${section}`);
+        this.homePageDescription = page.locator('#page-description');
+        this.sectionCards = page.locator('.section-cards');
+        this.sectionCard = (section) => this.sectionCards.getByTestId(`section-${section}`);
         this.openTestSectionButton = (section) => this.sectionCard(section).getByRole('button', { name: 'Open Test Section' });
         this.backButton = page.getByTestId('back-button');
+        this.logo = page.getByRole('banner').locator('#header-logo');
     }
 
     async navigate() {
@@ -30,6 +32,15 @@ class HomePage {
 
     async validateURL(expectedURL) {
         await expect(this.page).toHaveURL(expectedURL, { timeout: 10000 });
+    }
+
+    async validateLogoVisibility() {
+        await expect(this.logo).toBeVisible({ timeout: 10000 });
+    }
+
+    async validateDescriptionText() {
+        await expect(this.homePageDescription).toBeVisible({ timeout: 10000 });
+        await expect(this.homePageDescription).not.toBeEmpty();
     }
 }
 module.exports = HomePage;
